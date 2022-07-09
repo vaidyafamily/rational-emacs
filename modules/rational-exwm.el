1f9085e1f754156d82dab0fefdb11c06a51e99aa
Amol Vaidya <amolvaidya06@gmail.com>
2022-07-09 15:39:00 -0500

    Began EXWM module and set up a couple of customizable variables.
    rational-exwm-worskpace-by-class and rational-exwm-titles-by-class
    variables defined. The purpose of the former is to hold an alist that
    specifies the workspace particular classes of xwindows programs are to
    open. The purpose of the latter is to allow the user how they want
    particular classes of programs titled.

---
+++ modules/rational-exwm.el
@@ -0,0 +1,28 @@
+;;; rational-exwm.el --- EXWM configuration -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022
+;; SPDX-License-Identifier: MIT
+
+;; Author: System Crafters Community
+
+;;; Commentary:
+
+;; Use Emacs as your Windows Manager
+
+;;; Code:
+
+(defgroup rational-exwm '()
+  "EXWM related configuration for Rational Emacs."
+  :tag "Rational EXWM"
+  :group 'rational)
+ 
+;; Customizable configuration variables
+(defcustom rational-exwm-workspace-by-class '()
+  "Alist specifying which EXWM workspace a particular program should open on. Keys should be strings that contain the xwindows program class name. Values should be integers between 0 and 9, specifying which workspace a class opens on"
+  :type '(alist :key-type string :value-type integer)
+  :group 'rational-ui)
+
+(defcustom rational-exwm-titles-by-class '()
+  "Alist specifying how certain program classes should be named. Keys should be strings that contain the xwindows program class name. Values should be strings specifying how the class should be named."
+  :type '(alist :key-type string :value-type integer)
+  :group 'rational-ui)
