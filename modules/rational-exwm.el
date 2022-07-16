a649670f25eeed38dc1047df08f7b8044e2156f9
Amol Vaidya <amolvaidya06@gmail.com>
2022-07-16 13:25:30 -0500

    Added some EXWM input prefix keys.

--- modules/rational-exwm.el
+++ modules/rational-exwm.el
@@ -23,7 +23,7 @@
   :group 'rational-ui)
 
 (defcustom rational-exwm-titles-by-class '()
-  "Alist specifying how certain program classes should be named. Keys should be strings that contain the xwindows program class name. Values should be strings specifying how the class should be named."
+   "Alist specifying how certain program classes should be named. Keys should be strings that contain the xwindows program class name. Values should be strings specifying how the class should be named."
   :type '(alist :key-type string :value-type string)
   :group 'rational-ui)
 
@@ -36,3 +36,18 @@
   "Specifies whether or not the status of the battery should be displayed."
   :type boolean
   :group 'rational-ui)
+
+(customize-set-variable exwm-input-prefix-keys
+                      '(?\C-x
+			?\C-u
+			?\C-g
+			?\C-w
+			?\C-h
+			?\M-x
+			?\M-`
+			?\M-&
+			?\M-:
+			?\C-\M-j  ;; Buffer list
+			?\M-\ ))  ;; Ctrl+Space
+
+ 
